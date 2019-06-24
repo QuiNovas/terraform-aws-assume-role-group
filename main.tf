89f3102c5727433ab84ba7260634237e2311e39b
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-24 19:58:47 +0530

    terraform 0.12 upgrade

--- main.tf
+++ main.tf
@@ -1,39 +1,33 @@
 resource "aws_iam_group" "assume_role" {
-  name = "${var.name}"
+  name = var.name
 }
 
 data "aws_iam_policy_document" "group" {
-  "statement" {
+  statement {
     actions = [
-      "sts:AssumeRole"
+      "sts:AssumeRole",
     ]
-    resources = [
-      "${var.role_arns}"
-    ]
-    sid = "AllowAssumingOfRole"
+    resources = var.role_arns
+    sid       = "AllowAssumingOfRole"
   }
 
   statement {
     actions = [
-      "iam:ListAttachedRolePolicies"
+      "iam:ListAttachedRolePolicies",
     ]
-    resources = [
-      "${var.role_arns}"
-    ]
-    sid = "AllowListingOfRolePolicies"
+    resources = var.role_arns
+    sid       = "AllowListingOfRolePolicies"
   }
 }
 
 resource "aws_iam_group_policy" "group" {
-  group   = "${aws_iam_group.assume_role.id}"
-  name    = "role_access"
-  policy  = "${data.aws_iam_policy_document.group.json}"
+  group  = aws_iam_group.assume_role.id
+  name   = "role_access"
+  policy = data.aws_iam_policy_document.group.json
 }
 
 resource "aws_iam_group_membership" "group" {
-  group = "${aws_iam_group.assume_role.name}"
+  group = aws_iam_group.assume_role.name
   name  = "${var.name}-membership"
-  users = [
-    "${var.allowed_user_names}"
-  ]
+  users = var.allowed_user_names
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,15 @@
 variable "allowed_user_names" {
   description = "The names of the users to assign to the group"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "name" {
   description = "The name of the IAM group"
-  type        = "string"
+  type        = string
 }
 
 variable "role_arns" {
   description = "The roles that may be assumed"
-  type        = "list"
+  type        = list(string)
 }
+
